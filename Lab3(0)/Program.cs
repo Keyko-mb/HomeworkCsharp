c36d50b84e0f8d89b0e485dd7391a9b352b17c56
Алина Шафикова <shafikova-2003@mail.ru>
2023-11-08 20:29:37 +0400

    Finich last excercise

--- Lab3(0)/Program.cs
+++ Lab3(0)/Program.cs
@@ -1,66 +1,130 @@
-﻿using System.Diagnostics.SymbolStore;
+﻿
+using System.Linq;
 using System.Xml.Linq;
 
 XDocument doc;
-string uri = "C:\\Users\\shafi\\source\\repos\\Lab3\\Lab3(0)\\XMLFile1.xml";
+string uri = "C:\\Users\\shafi\\source\\repos\\HomeworkCsharp\\Lab3(0)\\XMLFile1.xml";
 doc = XDocument.Load(uri);
 
-Console.WriteLine("Задание 1");
-string searchName = "Петров Петр Петрович";
-bool jobSortReverse = false;
-int from = 2010;
-int to = 2023;
+Console.WriteLine("Введите: 1. Задания, 2. Редактировать XML");
+int task = Int32.Parse(Console.ReadLine());
+if (task == 1) Tasks.goTasks(doc);
+else Console.WriteLine("Введите: 1. Добавить сотруднику место работы и зарплату " +
+    "2. Изменить место работы и зарплату ");
+task = Int32.Parse(Console.ReadLine());
+if (task == 1)
+{
+    Console.WriteLine("Добавление места работы и зарплаты");
+    string name = "Петров Петр Петрович";
 
-var employee = doc.Element("Сотрудники").Elements("Сотрудник")
-    .Where(e => e.Element("ФИО").Value.Equals(searchName))
+    Console.WriteLine("Список работ и зарплат до: ");
+    var info = doc.Descendants("Сотрудник")
+    .Where(e => e.Element("ФИО").Value.Equals(name))
     .Select(e => new
     {
-        name = e.Element("ФИО").Value,
+        jobs = e.Element("Список_Работ").Elements("Работа"),
+        payments = e.Element("Список_Зарплат").Elements("Зарплата")
 
-        jobs = (jobSortReverse ?
-        e.Element("Список_Работ")
-        .Elements("Работа")
-        .OrderBy(work => work.Element("Дата_начала").Value.Split("-")[0])
-        .Select(work => work)
-        : e.Element("Список_Работ")
-        .Elements("Работа")
-        .OrderByDescending(work => work.Element("Дата_начала").Value.Split("-")[0])
-        .Select(work => work)),
-        
-        payments = e.Element("Список_Зарплат")
-        .Elements("Зарплата")
-        .Where(payment => (Int32.Parse(payment.Element("Год").Value) > from) && (Int32.Parse(payment.Element("Год").Value)) < to)
-        .Select(payment => payment),
-    }) ;
-
-foreach (var el in employee)
-{
-    Console.WriteLine(el.name);
-    foreach (var job in el.jobs) Console.WriteLine(job);
-    foreach (var payment in el.payments) Console.WriteLine(payment);
-}
-
-Console.WriteLine("\nЗадание 2");
-var departments = doc.Descendants("Работа")
-            .GroupBy(r => (string)r.Element("Отдел"))
-            .Select(group => new
-            {
-                DepartmentName = group.Key,
-                TotalEmployeeCount = group.Count(),
-                JobTitles = group.Select(r => (string)r.Element("Название")).Distinct().ToList(),
-                RealEmployeesCount = group.Count(r => r.Element("Дата_окончания").Value == "")
-            })
-            .ToList();
-
-foreach (var department in departments)
-{
-    double departmentShare = (double)department.RealEmployeesCount / department.TotalEmployeeCount * 100;
-    Console.WriteLine($"Отдел: {department.DepartmentName}");
-    Console.WriteLine($"Количество работающих сотрудников: {department.RealEmployeesCount}");
-    Console.WriteLine("Список должностей:");
-    foreach (var jobTitle in department.JobTitles)
+    });
+    foreach (var el in info)
     {
-        Console.WriteLine(jobTitle);
+        foreach (var job in el.jobs) Console.WriteLine(job);
+        foreach (var payment in el.payments) Console.WriteLine(payment);
     }
-    Console.WriteLine($"Доля работающих сотрудников от общего числа сотрудников отдела: {departmentShare}%\n");
+
+    string jobName = "Механик";
+    string dateFrom = "2023-06-15";
+    string dateTo = "2023-06-16";
+    string department = "Отдел по ремонту";
+    var newWorkPlace = new XElement("Работа",
+        new XElement("Название", jobName),
+        new XElement("Дата_начала", dateFrom),
+        new XElement("Дата_окончания", dateTo),
+        new XElement("Отдел", department)
+        );
+    doc.Element("Сотрудники").Elements("Сотрудник")
+        .First(e => e.Element("ФИО").Value.Equals(name))
+        .Element("Список_Работ")
+        .Add(newWorkPlace);
+    string year = "2023";
+    string month = "06";
+    string size = "55555";
+    var newPayment = new XElement("Зарплата",
+        new XElement("Год", year),
+        new XElement("Месяц", month),
+        new XElement("Размер", size)
+        );
+    doc.Element("Сотрудники").Elements("Сотрудник")
+        .First(e => e.Element("ФИО").Value.Equals(name))
+        .Element("Список_Зарплат")
+        .Add(newPayment);
+
+    doc.Save(uri);
+
+    Console.WriteLine("\nСписок работ и зарплат после: ");
+    info = doc.Descendants("Сотрудник")
+    .Where(e => e.Element("ФИО").Value.Equals(name))
+    .Select(e => new
+    {
+        jobs = e.Element("Список_Работ").Elements("Работа"),
+        payments = e.Element("Список_Зарплат").Elements("Зарплата")
+
+    });
+    foreach (var el in info)
+    {
+        foreach (var job in el.jobs) Console.WriteLine(job);
+        foreach (var payment in el.payments) Console.WriteLine(payment);
+    }
+}
+else
+{
+    Console.WriteLine("Редактирование места работы и зарплаты");
+    string name = "Петров Петр Петрович";
+
+    Console.WriteLine("Список работ и зарплат до: ");
+    var info = doc.Descendants("Сотрудник")
+    .Where(e => e.Element("ФИО").Value.Equals(name))
+    .Select(e => new
+    {
+        jobs = e.Element("Список_Работ").Elements("Работа"),
+        payments = e.Element("Список_Зарплат").Elements("Зарплата")
+
+    });
+    foreach (var el in info)
+    {
+        foreach (var job in el.jobs) Console.WriteLine(job);
+        foreach (var payment in el.payments) Console.WriteLine(payment);
+    }
+
+    string oldJobName = "Директор";
+    string newJobName = "Измененная должность";
+    doc.Element("Сотрудники").Elements("Сотрудник")
+        .First(e => e.Element("ФИО").Value.Equals(name))
+        .Element("Список_Работ").Elements("Работа")
+        .Where(work => work.Element("Название").Value.Equals(oldJobName)).First()
+        .SetElementValue("Название", newJobName);
+    string oldSize = "30000";
+    string newSize = "11111";
+    doc.Element("Сотрудники").Elements("Сотрудник")
+        .First(e => e.Element("ФИО").Value.Equals(name))
+        .Element("Список_Зарплат").Elements("Зарплата")
+        .Where(payment => payment.Element("Размер").Value.Equals(oldSize)).First()
+        .SetElementValue("Размер", newSize);
+
+    doc.Save(uri);
+
+    Console.WriteLine("\nСписок работ и зарплат после: ");
+    info = doc.Descendants("Сотрудник")
+    .Where(e => e.Element("ФИО").Value.Equals(name))
+    .Select(e => new
+    {
+        jobs = e.Element("Список_Работ").Elements("Работа"),
+        payments = e.Element("Список_Зарплат").Elements("Зарплата")
+
+    });
+    foreach (var el in info)
+    {
+        foreach (var job in el.jobs) Console.WriteLine(job);
+        foreach (var payment in el.payments) Console.WriteLine(payment);
+    }
 }
---
+++ Lab3(0)/Tasks.cs
@@ -0,0 +1,126 @@
+﻿using System.Xml.Linq;
+
+class Tasks
+{
+    public static void goTasks(XDocument doc)
+    {
+        Console.WriteLine("Задание 1");
+        string searchName = "Петров Петр Петрович";
+        bool jobSortReverse = false;
+        int from = 2010;
+        int to = 2023;
+        var employee = doc.Element("Сотрудники").Elements("Сотрудник")
+            .Where(e => e.Element("ФИО").Value.Equals(searchName))
+            .Select(e => new
+            {
+                name = e.Element("ФИО").Value,
+
+                jobs = (jobSortReverse ?
+                e.Element("Список_Работ")
+                .Elements("Работа")
+                .OrderBy(work => work.Element("Дата_начала").Value.Split("-")[0])
+                .Select(work => work)
+                : e.Element("Список_Работ")
+                .Elements("Работа")
+                .OrderByDescending(work => work.Element("Дата_начала").Value.Split("-")[0])
+                .Select(work => work)),
+
+                payments = e.Element("Список_Зарплат")
+                .Elements("Зарплата")
+                .Where(payment => (Int32.Parse(payment.Element("Год").Value) > from) && (Int32.Parse(payment.Element("Год").Value)) < to)
+                .Select(payment => payment),
+            });
+        foreach (var el in employee)
+        {
+            Console.WriteLine(el.name);
+            foreach (var job in el.jobs) Console.WriteLine(job);
+            foreach (var payment in el.payments) Console.WriteLine(payment);
+        }
+
+
+        Console.WriteLine("\nЗадание 2");
+        var departments = doc.Descendants("Работа")
+                    .GroupBy(work => (string)work.Element("Отдел"))
+                    .Select(group => new
+                    {
+                        DepartmentName = group.Key,
+                        TotalEmployeeCount = group.Count(),
+                        JobTitles = group.Select(work => (string)work.Element("Название")).Distinct().ToList(),
+                        RealEmployeesCount = group.Count(work => work.Element("Дата_окончания").Value == "")
+                    })
+                    .ToList();
+        foreach (var department in departments)
+        {
+            double departmentShare = (double)department.RealEmployeesCount / department.TotalEmployeeCount * 100;
+            Console.WriteLine($"Отдел: {department.DepartmentName}");
+            Console.WriteLine($"Количество работающих сотрудников: {department.RealEmployeesCount}");
+            Console.WriteLine("Список должностей:");
+            foreach (var jobTitle in department.JobTitles)
+            {
+                Console.WriteLine(jobTitle);
+            }
+            Console.WriteLine($"Доля работающих сотрудников от общего числа сотрудников отдела: {departmentShare}%\n");
+        }
+
+
+        Console.WriteLine("\nЗадание 3");
+        var employees = doc.Element("Сотрудники").Elements("Сотрудник")
+                    .Where(e => e.Elements("Список_Работ").Elements("Работа")
+                        .Count(work => work.Element("Дата_окончания").Value == "") > 1)
+                    .GroupBy(e => e.Element("ФИО").Value)
+                    .Select(g => new
+                    {
+                        name = g.Key,
+                        departments = g.SelectMany(e => e.Descendants("Работа")
+                        .Where(work => work.Element("Дата_окончания").Value == "")
+                        .Select(work => work.Element("Отдел").Value)),
+                        maxSalary = g.SelectMany(e => e.Descendants("Зарплата")
+                        .Select(salary => (decimal)salary.Element("Размер")))
+                        .Max()
+                    });
+        foreach (var e in employees)
+        {
+            Console.WriteLine($"Сотрудник: {e.name}");
+            Console.WriteLine("Отделы: ");
+            foreach (var department in e.departments) Console.WriteLine(department);
+            Console.WriteLine($"Максимальная зарплата: {e.maxSalary}");
+        }
+
+
+        Console.WriteLine("\nЗадание 4");
+        var departments2 = doc.Element("Сотрудники").Elements("Сотрудник")
+            .SelectMany(e => e.Element("Список_Работ").Elements("Работа"))
+            .GroupBy(work => work.Element("Отдел").Value)
+            .Where(g => g.Count() <= 3)
+            .Select(g => g.Key);
+
+        foreach (var department in departments2) Console.WriteLine(department);
+
+
+        Console.WriteLine("\nЗадание 5");
+        var hiringYears = doc.Element("Сотрудники").Elements("Сотрудник")
+            .SelectMany(e => e.Element("Список_Работ").Elements("Работа"))
+            .GroupBy(work => work.Element("Дата_начала").Value.Split("-")[0])
+            .OrderByDescending(g => g.Count()).Take(1)
+            .Select(g => new
+            {
+                Year = g.Key,
+                Count = g.Count()
+            });
+
+        var firingYears = doc.Element("Сотрудники").Elements("Сотрудник")
+            .SelectMany(e => e.Element("Список_Работ").Elements("Работа"))
+            .Where(e => e.Element("Дата_окончания").Value != "")
+            .GroupBy(r => r.Element("Дата_окончания").Value.Split("-")[0])
+            .OrderByDescending(g => g.Count()).Take(1)
+            .Select(g => new
+            {
+                Year = g.Key,
+                Count = g.Count()
+            });
+        foreach (var year in hiringYears)
+            Console.WriteLine($"Год : {year.Year} Кол-во нанятых: {year.Count}");
+        foreach (var year in firingYears)
+            Console.WriteLine($"Год : {year.Year} Кол-во уволенных: {year.Count}");
+    }
+}
